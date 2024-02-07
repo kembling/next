54ece43b2aaf8421106875512e8ad3ac498ef29d
kembling <andrewkembo91@gmail.com>
2024-02-07 14:31:03 +0200

    completed

---
+++ next.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Example of JavaScript function for button click
+    $("#signup-btn").click(function() {
+      alert("Thank you for signing up! We'll get in touch with you shortly.");
+    });
+  });
